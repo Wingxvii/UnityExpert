54b4b20cff57e60b02cab84c24529862ea9a959c
Unknown <john.wang@uoit.net>
2019-05-26 00:36:17 -0400

    on AI

--- Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -4,9 +4,15 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets._2D
 {
-    [RequireComponent(typeof (PlatformerCharacter2D))]
+    [RequireComponent(typeof(PlatformerCharacter2D))]
     public class Platformer2DUserControl : MonoBehaviour
     {
+        public enum Runner
+        {
+            Player, Computer
+        }
+
+        public Runner runState;
         private PlatformerCharacter2D m_Character;
         private bool m_Jump;
 
@@ -26,12 +32,41 @@ namespace UnityStandardAssets._2D
 
         private void FixedUpdate()
         {
-            // Read the inputs.
-            bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            // Pass all parameters to the character control script.
-            m_Character.Move(h, crouch, m_Jump);
+
+            if (runState == Runner.Computer)
+            {
+                AutoRunFixedUpdate();
+            }
+            else if(runState == Runner.Player) {
+                // Read the inputs.
+                bool crouch = Input.GetKey(KeyCode.LeftControl);
+                float h = CrossPlatformInputManager.GetAxis("Horizontal");
+                // Pass all parameters to the character control script.
+                m_Character.Move(h, crouch, m_Jump);
+            }
+
+
             m_Jump = false;
         }
+
+        #region AutoRun
+        private void AutoRunFixedUpdate() {
+
+            float h = 0.0f;
+            bool crouch = false;
+            bool jump = false;
+
+            //ground check
+            if (m_Character.IsOnGround())
+            {
+
+            }
+
+            m_Character.Move(h, crouch, jump);
+        }
+
+
+        #endregion
     }
+
 }
